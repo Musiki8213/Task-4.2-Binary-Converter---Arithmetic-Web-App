588422db0930d125220341e5a007aa477190a492
Musiki8213 <sithomolamusiki@gmail.com>
2025-08-04 15:51:49 +0200

    worked on funtionality of buttons

---
+++ converter.js
@@ -0,0 +1,76 @@
+
+function hideAllForms() {
+  fileForm.style.display = 'none';
+  temForm.style.display = 'none';
+  scieForm.style.display = 'none';
+  metricform.style.display = 'none';
+}
+
+
+//FILE TRANSFER
+const fileBtn = document.getElementById("FileTransferBtn")
+const fileForm = document.getElementById("fileTransDiv")
+
+
+function fileShow(){
+    hideAllForms()
+     if (fileForm.style.display === 'none' || fileForm.style.display === '') {
+      fileForm.style.display = 'block';
+      
+    }
+    else {
+      fileForm.style.display = 'none';
+    }
+}
+
+//TEMPERATURE
+
+
+const tempBtn = document.getElementById("TempetatureBtn")
+const temForm = document.getElementById("tempDiv")
+
+
+function tempShow(){
+    hideAllForms()
+     if (temForm.style.display === 'none' || temForm.style.display === '') {
+      temForm.style.display = 'block';
+    }
+    else {
+      temForm.style.display = 'none';
+    }
+}
+
+//SCIENTIFIC
+
+
+const scieBtn = document.getElementById("ScientificBtn")
+const scieForm = document.getElementById("scientDiv")
+
+
+function scienShow(){
+    hideAllForms()
+     if (scieForm.style.display === 'none' || scieForm.style.display === '') {
+      scieForm.style.display = 'block';
+    }
+    else {
+      scieForm.style.display = 'none';
+    }
+}
+
+
+
+//METRIC
+
+const metricBtn = document.getElementById("MetricBtn")
+const metricform = document.getElementById("metricDiv")
+
+
+function metricShow(){
+    hideAllForms()
+     if (metricform.style.display === 'none' || metricform.style.display === '') {
+      metricform.style.display = 'block';
+    }
+    else {
+      metricform.style.display = 'none';
+    }
+}
